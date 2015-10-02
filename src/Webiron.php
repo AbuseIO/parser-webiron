46b8eebf7db7f58b3f42d7003d883031d70e27d5
Bart Vrancken <bart@abuse.io>
2015-10-02 20:56:47 +0200

    update new layout

--- src/Webiron.php
+++ src/Webiron.php
@@ -2,9 +2,6 @@
 
 namespace AbuseIO\Parsers;
 
-use ReflectionClass;
-use Log;
-
 class Webiron extends Parser
 {
     /**
@@ -12,7 +9,7 @@ class Webiron extends Parser
      */
     public function __construct($parsedMail, $arfMail)
     {
-        parent::__construct($parsedMail, $arfMail);
+        parent::__construct($parsedMail, $arfMail, $this);
     }
 
     /**
@@ -22,17 +19,6 @@ class Webiron extends Parser
      */
     public function parse()
     {
-        // Generalize the local config based on the parser class name.
-        $reflect = new ReflectionClass($this);
-        $this->configBase = 'parsers.' . $reflect->getShortName();
-
-        Log::info(
-            get_class($this). ': Received message from: '.
-            $this->parsedMail->getHeader('from') . " with subject: '" .
-            $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$this->configBase}.parser.name")
-        );
-
         /**
          *  Try to find ARF report.
          *  Some notification emails do not contain an ARF report. Instead they
@@ -40,6 +26,7 @@ class Webiron extends Parser
          *  and parse the email body.
          */
         $foundArf = false;
+
         foreach ($this->parsedMail->getAttachments() as $attachment) {
             // Only use the Webiron formatted reports, skip all others
             if (preg_match(config("{$this->configBase}.parser.report_file"), $attachment->filename)) {
@@ -49,8 +36,12 @@ class Webiron extends Parser
                 if (!empty($raw_report)) {
                     $foundArf = true;
 
-                    preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $raw_report, $matches);
-                    $report = array_combine($matches[1], array_map('trim', $matches[2]));
+                    if (preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $raw_report, $matches)) {
+                        $report = array_combine($matches[1], array_map('trim', $matches[2]));
+                    } else {
+                        $this->warningCount++;
+                        continue;
+                    }
 
                     if (!empty($report['Report-Type'])) {
                         $this->feedName = $report['Report-Type'];
@@ -88,34 +79,37 @@ class Webiron extends Parser
 
             // If feed is known and enabled, validate data and save report
             if ($this->isKnownFeed() && $this->isEnabledFeed()) {
-                preg_match_all('/  - ([^:]+): ([^\n]+)\n/', $body, $matches);
-                $report = array_combine($matches[1], array_map('trim', $matches[2]));
+                if (preg_match_all('/  - ([^:]+): ([^\n]+)\n/', $body, $matches)) {
+                    $report = array_combine($matches[1], array_map('trim', $matches[2]));
 
-                // Get IP address
-                preg_match(
-                    '/(?:Offending|Source) IP:[ ]+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\n/',
-                    $body,
-                    $matches
-                );
-                if (count($matches) == 2 && empty($report['ip'])) {
-                    $report['ip'] = $matches[1];
-                }
+                    // Get IP address
+                    preg_match(
+                        '/(?:Offending|Source) IP:[ ]+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\n/',
+                        $body,
+                        $matches
+                    );
+                    if (count($matches) == 2 && empty($report['ip'])) {
+                        $report['ip'] = $matches[1];
+                    }
 
-                // Sanity check
-                if ($this->hasRequiredFields($report) === true) {
-                    // Event has all requirements met, filter and add!
-                    $report = $this->applyFilters($report);
+                    // Sanity check
+                    if ($this->hasRequiredFields($report) === true) {
+                        // Event has all requirements met, filter and add!
+                        $report = $this->applyFilters($report);
 
-                    $this->events[] = [
-                        'source'        => config("{$this->configBase}.parser.name"),
-                        'ip'            => $report['ip'],
-                        'domain'        => false,
-                        'uri'           => false,
-                        'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
-                        'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
-                        'timestamp'     => strtotime($report['Time']),
-                        'information'   => json_encode($report),
-                    ];
+                        $this->events[] = [
+                            'source'        => config("{$this->configBase}.parser.name"),
+                            'ip'            => $report['ip'],
+                            'domain'        => false,
+                            'uri'           => false,
+                            'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
+                            'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
+                            'timestamp'     => strtotime($report['Time']),
+                            'information'   => json_encode($report),
+                        ];
+                    }
+                } else {
+                    $this->warningCount++;
                 }
             }
         }
